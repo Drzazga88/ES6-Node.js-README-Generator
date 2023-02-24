3fb82dbfe200963fa9b7d52b7442a295f36dee21
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-02-24 19:18:10 +0000

    Array of questions for user added

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "Title",
+    question: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    question: "What is the description of the project?",
+  },
+  {
+    type: "input",
+    name: "Installation",
+    question: "Provide installation instructions for the project. ",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    question: "Provide usage instructions for the project.",
+  },
+  {
+    type: "list",
+    name: "Licence",
+    question: "What licence do you want to use?",
+    choices: ["MIT", "GPL", "BSD", "None"],
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    question: "Provide contribution guide for the project.",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    question: "Provide testing instructions for the project.",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
